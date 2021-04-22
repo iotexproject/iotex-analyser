1dc25a83cd547f6818f39937f2e789909bf78eeb
millken <millken@gmail.com>
2021-04-22 14:16:58 +0800

    rename account_balance_erc20 to token_erc20

--- plugins/token_erc20/token_erc20.go
+++ plugins/token_erc20/token_erc20.go
@@ -11,25 +11,26 @@ import (
 	"github.com/pkg/errors"
 )
 
-const VERSION = "1.0.1"
+const VERSION = "1.1.0"
 
-type accountBalancePlugin struct {
+type tokenPlugin struct {
 	tableName string
 }
 
-func (b accountBalancePlugin) Name() string {
-	return "account_balance_erc20"
+func (b tokenPlugin) Name() string {
+	return "token_erc20"
 }
 
-func (b accountBalancePlugin) Type() plugin.Type {
+func (b tokenPlugin) Type() plugin.Type {
 	return plugin.TypeStandard
 }
 
-func (b accountBalancePlugin) Start(ctx context.Context) error {
+func (b tokenPlugin) Start(ctx context.Context) error {
 	createSql := "CREATE TABLE IF NOT EXISTS `" + b.tableName + "` (" +
 		"`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT," +
 		"`block_height` bigint(20) unsigned NOT NULL DEFAULT '0'," +
 		"`action_hash` varchar(64) NOT NULL DEFAULT ''," +
+		"`contract_address` varchar(41) NOT NULL DEFAULT ''," +
 		"`amount` DECIMAL(42, 0) UNSIGNED NOT NULL DEFAULT 0," +
 		"`from` varchar(41) NOT NULL DEFAULT ''," +
 		"`to` varchar(41) NOT NULL DEFAULT ''," +
@@ -37,6 +38,7 @@ func (b accountBalancePlugin) Start(ctx context.Context) error {
 		"KEY `block_height` (`block_height`)," +
 		"KEY `from` (`from`)," +
 		"KEY `to` (`to`)," +
+		"KEY `contract_address` (`contract_address`)," +
 		"KEY `action_hash` (`action_hash`(9))" +
 		") ENGINE=InnoDB DEFAULT CHARSET=latin1;"
 	if _, err := kernel.GetDB().Exec(createSql); err != nil {
@@ -45,7 +47,7 @@ func (b accountBalancePlugin) Start(ctx context.Context) error {
 	return nil
 }
 
-func (b accountBalancePlugin) PutBlock(ctx context.Context, blk *block.Block) error {
+func (b tokenPlugin) PutBlock(ctx context.Context, blk *block.Block) error {
 	err := kernel.Transaction(func(tx *sql.Tx) error {
 
 		for _, receipt := range blk.Receipts {
@@ -68,11 +70,12 @@ func (b accountBalancePlugin) PutBlock(ctx context.Context, blk *block.Block) er
 					return errors.Wrap(err, "failed to parse contract data")
 				}
 				insertData := map[string]interface{}{
-					"block_height": blk.Height(),
-					"action_hash":  actionHash,
-					"amount":       amount,
-					"from":         from,
-					"to":           to,
+					"block_height":     blk.Height(),
+					"action_hash":      actionHash,
+					"amount":           amount,
+					"contract_address": log.Address,
+					"from":             from,
+					"to":               to,
 				}
 				if err := kernel.InsertTableData(tx, b.tableName, insertData); err != nil {
 					return errors.Wrap(err, "failed to insert table data")
@@ -86,15 +89,15 @@ func (b accountBalancePlugin) PutBlock(ctx context.Context, blk *block.Block) er
 	return err
 }
 
-func (b accountBalancePlugin) Stop(ctx context.Context) error {
+func (b tokenPlugin) Stop(ctx context.Context) error {
 	return nil
 }
 
-func (b accountBalancePlugin) Version() string {
+func (b tokenPlugin) Version() string {
 	return VERSION
 }
 
 // exported
-var Plugin = accountBalancePlugin{
-	tableName: "account_balance_erc20",
+var Plugin = tokenPlugin{
+	tableName: "token_erc20",
 }

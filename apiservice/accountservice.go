aae089fb3cba84954e86e08c6f1336b1c0e97197
millken <millken@gmail.com>
2021-05-28 11:05:32 +0800

    update account  balance grpc api

--- apiservice/accountservice.go
+++ apiservice/accountservice.go
@@ -15,108 +15,101 @@ type AccountService struct {
 	api.UnimplementedAccountServiceServer
 }
 
-/*
-mysql> SELECT SUM(amount),SUM(gas_price*gas_consumed) FROM block_action where block_height<=8844615 and `from`='io1fuhhg9jgdxwpms9dsdfwjdc90nt7v67hx40cd8';
-+----------------------+-----------------------------+
-| SUM(amount)          | SUM(gas_price*gas_consumed) |
-+----------------------+-----------------------------+
-| 33000000000000000000 |        24801116000000000000 |
-+----------------------+-----------------------------+
-1 row in set (0.02 sec)
-mysql> SELECT SUM(amount) FROM block_action where block_height<=8844615 and `to`='io1fuhhg9jgdxwpms9dsdfwjdc90nt7v67hx40cd8';
-+------------------------+
-| SUM(amount)            |
-+------------------------+
-| 1020000000000000000000 |
-+------------------------+
-1 row in set (0.01 sec)
-*/
+//curl -d '{"request":[{"address": "io1ryztljunahyml9s7atfwtsx7s8wvr5maufa6zp", "height":8927781 }]}' http://127.0.0.1:7778/api.AccountService.GetIotexBalanceByHeight
 func (s *AccountService) GetIotexBalanceByHeight(ctx context.Context, req *api.AccountRequest) (*api.AccountResponse, error) {
 	resp := &api.AccountResponse{}
-	addr := req.GetAddress()
-	height := req.GetHeight()
+	db := kernel.GetDB()
+	for _, req := range req.GetRequest() {
+		addr := req.GetAddress()
+		height := req.GetHeight()
+		if addr[:2] == "0x" || addr[:2] == "0X" {
+			add, err := address.FromHex(addr)
+			if err != nil {
+				return nil, err
+			}
 
-	if addr[:2] == "0x" || addr[:2] == "0X" {
-		add, err := address.FromHex(addr)
+			addr = add.String()
+		}
+		var amount sql.NullString
+		query := "SELECT sum(in_flow)-sum(out_flow) from account_income WHERE block_height<=? and account_address=?"
+		err := db.QueryRow(query, height, addr).Scan(&amount)
 		if err != nil {
 			return nil, err
 		}
-
-		addr = add.String()
+		balance, ok := big.NewInt(0).SetString(amount.String, 10)
+		if !ok {
+			balance = big.NewInt(0)
+		}
+		res := &api.BalanceResponse{
+			Address: req.GetAddress(),
+			Height:  req.GetHeight(),
+			Balance: util.RauToString(balance, util.IotxDecimalNum),
+		}
+		resp.Response = append(resp.Response, res)
 	}
 
-	db := kernel.GetDB()
-
-	var amount sql.NullString
-	query := "SELECT sum(in_flow)-sum(out_flow) from account_income WHERE block_height<=? and account_address=?"
-	err := db.QueryRow(query, height, addr).Scan(&amount)
-	if err != nil {
-		return nil, err
-	}
-
-	balance, ok := big.NewInt(0).SetString(amount.String, 10)
-	if !ok {
-		balance = big.NewInt(0)
-	}
-	resp.Balance = util.RauToString(balance, util.IotxDecimalNum)
 	return resp, nil
 }
 
 //grpcurl -plaintext -d '{"address": "io1ryztljunahyml9s7atfwtsx7s8wvr5maufa6zp", "height":8927781 }' 127.0.0.1:7777 api.AccountService.GetErc20TokenBalanceByHeight
-//curl -d '{"address": "io1ryztljunahyml9s7atfwtsx7s8wvr5maufa6zp", "height":8927781 }' http://rvice.GetErc20TokenBalanceByHeight
+//curl -d '{"request":[{"address": "io1ryztljunahyml9s7atfwtsx7s8wvr5maufa6zp", "contract_address": "io1ryztljunahyml9s7atfwtsx7s8wvr5maufa6zp", "height":8927781 }]}' http://127.0.0.1:7778/api.AccountService.GetErc20TokenBalanceByHeight
 func (s *AccountService) GetErc20TokenBalanceByHeight(ctx context.Context, req *api.AccountErc20TokenRequest) (*api.AccountResponse, error) {
 	resp := &api.AccountResponse{}
-	addr := req.GetAddress()
-	height := req.GetHeight()
-	contractAddress := req.GetContractAddress()
-
-	if len(addr) > 2 && (addr[:2] == "0x" || addr[:2] == "0X") {
-		add, err := address.FromHex(addr)
-		if err != nil {
-			return nil, err
-		}
-
-		addr = add.String()
-	}
-
-	if len(contractAddress) > 2 && (contractAddress[:2] == "0x" || contractAddress[:2] == "0X") {
-		add, err := address.FromHex(contractAddress)
-		if err != nil {
-			return nil, err
-		}
-
-		contractAddress = add.String()
-	}
-
 	db := kernel.GetDB()
+	for _, req := range req.GetRequest() {
+		addr := req.GetAddress()
+		height := req.GetHeight()
+		contractAddress := req.GetContractAddress()
+		if len(addr) > 2 && (addr[:2] == "0x" || addr[:2] == "0X") {
+			add, err := address.FromHex(addr)
+			if err != nil {
+				return nil, err
+			}
 
-	//get receive amount
-	var toAmount sql.NullString
-	query := "SELECT SUM(amount) FROM token_erc20 WHERE block_height<=? AND `to`=? AND `contract_address`=?"
-	err := db.QueryRow(query, height, addr, contractAddress).Scan(&toAmount)
-	if err != nil {
-		return nil, err
+			addr = add.String()
+		}
+
+		if len(contractAddress) > 2 && (contractAddress[:2] == "0x" || contractAddress[:2] == "0X") {
+			add, err := address.FromHex(contractAddress)
+			if err != nil {
+				return nil, err
+			}
+
+			contractAddress = add.String()
+		}
+		//get receive amount
+		var toAmount sql.NullString
+		query := "SELECT SUM(amount) FROM token_erc20 WHERE block_height<=? AND `to`=? AND `contract_address`=?"
+		err := db.QueryRow(query, height, addr, contractAddress).Scan(&toAmount)
+		if err != nil {
+			return nil, err
+		}
+
+		//get cost amount
+		var fromAmount sql.NullString
+		query = "SELECT SUM(amount) FROM token_erc20 WHERE block_height<=? AND `from`=? AND `contract_address`=?"
+		err = db.QueryRow(query, height, addr, contractAddress).Scan(&fromAmount)
+		if err != nil {
+			return nil, err
+		}
+
+		to, ok := big.NewInt(0).SetString(toAmount.String, 10)
+		if !ok {
+			to = big.NewInt(0)
+		}
+		from, ok := big.NewInt(0).SetString(fromAmount.String, 10)
+		if !ok {
+			from = big.NewInt(0)
+		}
+		balance := new(big.Int).Sub(to, from)
+		res := &api.BalanceResponse{
+			Address:         req.GetAddress(),
+			ContractAddress: req.GetContractAddress(),
+			Height:          req.GetHeight(),
+			Balance:         util.RauToString(balance, 6),
+		}
+		resp.Response = append(resp.Response, res)
 	}
 
-	//get cost amount
-	var fromAmount sql.NullString
-	query = "SELECT SUM(amount) FROM token_erc20 WHERE block_height<=? AND `from`=? AND `contract_address`=?"
-	err = db.QueryRow(query, height, addr, contractAddress).Scan(&fromAmount)
-	if err != nil {
-		return nil, err
-	}
-
-	to, ok := big.NewInt(0).SetString(toAmount.String, 10)
-	if !ok {
-		to = big.NewInt(0)
-	}
-	from, ok := big.NewInt(0).SetString(fromAmount.String, 10)
-	if !ok {
-		from = big.NewInt(0)
-	}
-
-	balance := new(big.Int).Sub(to, from)
-
-	resp.Balance = util.RauToString(balance, 6)
 	return resp, nil
 }

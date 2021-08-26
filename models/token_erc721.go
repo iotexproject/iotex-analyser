ef07bcb18931a378e821491dc541a88a7afe4d3c
millken <millken@gmail.com>
2021-08-26 15:38:35 +0800

    fix token amount size

--- models/token_erc721.go
+++ models/token_erc721.go
@@ -11,7 +11,7 @@ type TokenErc721 struct {
 	BlockHeight     uint64          `gorm:"unsigned;index" sql:"type:bigint"`
 	ActionHash      string          `gorm:"size:64;not null;index:,length:9"`
 	ContractAddress string          `gorm:"size:42;not null;default:'';index:,length:9"`
-	Amount          decimal.Decimal `gorm:"type:decimal(64,0);not null;default:0;"`
+	Amount          decimal.Decimal `gorm:"type:decimal(128,0);not null;default:0;"`
 	Sender          string          `gorm:"size:42;not null;default:'';index:,length:9"`
 	Recipient       string          `gorm:"size:42;not null;default:'';index:,length:9"`
 	Timestamp       time.Time       `gorm:"type:timestamp;"`

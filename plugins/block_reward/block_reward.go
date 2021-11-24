7fe469a68ed48fe08d224e33d5e628c3b4f45c2e
millken <millken@gmail.com>
2021-11-24 14:27:36 +0800

    fix missing epochReward,foundationBonus in block_reward plugin

--- plugins/block_reward/block_reward.go
+++ plugins/block_reward/block_reward.go
@@ -21,7 +21,7 @@ import (
 	"gorm.io/gorm"
 )
 
-const VERSION = "2.0.0"
+const VERSION = "2.1.0"
 
 type blockRewardPlugin struct {
 }
@@ -49,63 +49,63 @@ func (b blockRewardPlugin) Start(ctx context.Context) error {
 }
 
 func (b blockRewardPlugin) PutBlock(ctx context.Context, blk *block.Block) error {
-	blkHeight := blk.Height()
-	epochNum := kernel.GetEpochNum(blkHeight)
+	err := db.DB().Transaction(func(tx *gorm.DB) error {
+		blkHeight := blk.Height()
+		epochNum := kernel.GetEpochNum(blkHeight)
 
-	grantRewardActs := make(map[hash.Hash256]bool)
-	// log action index
-	for _, selp := range blk.Actions {
-		if _, ok := selp.Action().(*action.GrantReward); ok {
-			actHash := selp.Hash()
-			grantRewardActs[actHash] = true
-		}
-	}
-	// log receipt index
-	for _, receipt := range blk.Receipts {
-		if _, ok := grantRewardActs[receipt.ActionHash]; !ok {
-			continue
-		}
-		// Parse receipt of grant reward
-		rewardInfoMap := make(map[string]*RewardInfo)
-		for _, l := range receipt.Logs() {
-			rewardLog := &rewardingpb.RewardLog{}
-			if err := proto.Unmarshal(l.Data, rewardLog); err != nil {
-				return errors.Wrap(err, "failed to unmarshal receipt data into reward log")
+		grantRewardActs := make(map[hash.Hash256]bool)
+		// log action index
+		for _, selp := range blk.Actions {
+			if _, ok := selp.Action().(*action.GrantReward); ok {
+				actHash := selp.Hash()
+				grantRewardActs[actHash] = true
 			}
-			rewards, ok := rewardInfoMap[rewardLog.Addr]
-			if !ok {
-				rewardInfoMap[rewardLog.Addr] = &RewardInfo{
-					BlockReward:     big.NewInt(0),
-					EpochReward:     big.NewInt(0),
-					FoundationBonus: big.NewInt(0),
+		}
+		// log receipt index
+		for _, receipt := range blk.Receipts {
+			if _, ok := grantRewardActs[receipt.ActionHash]; !ok {
+				continue
+			}
+			// Parse receipt of grant reward
+			rewardInfoMap := make(map[string]*RewardInfo)
+			for _, l := range receipt.Logs() {
+				rewardLog := &rewardingpb.RewardLog{}
+				if err := proto.Unmarshal(l.Data, rewardLog); err != nil {
+					return errors.Wrap(err, "failed to unmarshal receipt data into reward log")
 				}
-				rewards = rewardInfoMap[rewardLog.Addr]
+				rewards, ok := rewardInfoMap[rewardLog.Addr]
+				if !ok {
+					rewardInfoMap[rewardLog.Addr] = &RewardInfo{
+						BlockReward:     big.NewInt(0),
+						EpochReward:     big.NewInt(0),
+						FoundationBonus: big.NewInt(0),
+					}
+					rewards = rewardInfoMap[rewardLog.Addr]
+				}
+				amount, ok := big.NewInt(0).SetString(rewardLog.Amount, 10)
+				if !ok {
+					return errors.New("failed to convert reward amount from string to big int")
+				}
+				switch rewardLog.Type {
+				case rewardingpb.RewardLog_BLOCK_REWARD:
+					rewards.BlockReward.Add(rewards.BlockReward, amount)
+				case rewardingpb.RewardLog_EPOCH_REWARD:
+					rewards.EpochReward.Add(rewards.EpochReward, amount)
+				case rewardingpb.RewardLog_FOUNDATION_BONUS:
+					rewards.FoundationBonus.Add(rewards.FoundationBonus, amount)
+				default:
+					return errors.New("Unknown type of reward")
+				}
 			}
-			amount, ok := big.NewInt(0).SetString(rewardLog.Amount, 10)
-			if !ok {
-				return errors.New("failed to convert reward amount from string to big int")
-			}
-			switch rewardLog.Type {
-			case rewardingpb.RewardLog_BLOCK_REWARD:
-				rewards.BlockReward.Add(rewards.BlockReward, amount)
-			case rewardingpb.RewardLog_EPOCH_REWARD:
-				rewards.EpochReward.Add(rewards.EpochReward, amount)
-			case rewardingpb.RewardLog_FOUNDATION_BONUS:
-				rewards.FoundationBonus.Add(rewards.FoundationBonus, amount)
-			default:
-				return errors.New("Unknown type of reward")
-			}
-		}
 
-		if len(rewardInfoMap) == 0 {
-			continue
-		}
-		err := db.DB().Transaction(func(tx *gorm.DB) error {
+			if len(rewardInfoMap) == 0 {
+				continue
+			}
 
 			for addr, reward := range rewardInfoMap {
 				var cand models.Candidate
 				if err := tx.Model(cand).Where("reward_address=?", addr).Order("id desc").Take(&cand).Error; err != nil {
-					log.L().Warn("can not fetch candidate name", zap.String("reward_address", addr))
+					log.L().Warn("cannot fetch candidate name", zap.String("reward_address", addr))
 				}
 				m := models.BlockReward{
 					BlockHeight:     blkHeight,
@@ -121,12 +121,11 @@ func (b blockRewardPlugin) PutBlock(ctx context.Context, blk *block.Block) error
 					return err
 				}
 			}
-			return db.UpdateIndexHeightByTx(tx, b.Name(), blk.Height())
-		})
-		return err
-	}
+		}
+		return nil
+	})
+	return err
 
-	return nil
 }
 
 func (b blockRewardPlugin) Stop(ctx context.Context) error {

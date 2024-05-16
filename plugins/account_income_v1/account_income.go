1ce35cb0092a237510f04ded490deaccfb6bdfa5
hunshenshi <289517357@qq.com>
2024-05-16 21:34:49 +0800

    feat(plugin): add batch write in account_income_v1

--- plugins/account_income_v1/account_income.go
+++ plugins/account_income_v1/account_income.go
@@ -3,15 +3,23 @@ package main
 import (
 	"context"
 	"math/big"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/shopspring/decimal"
+	"go.uber.org/zap"
+	"gopkg.in/yaml.v2"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 
 	"github.com/iotexproject/iotex-address/address"
 	"github.com/iotexproject/iotex-analyser/config"
 	"github.com/iotexproject/iotex-analyser/db"
+	"github.com/iotexproject/iotex-analyser/kernel"
 	"github.com/iotexproject/iotex-analyser/plugin"
+	"github.com/iotexproject/iotex-analyser/server"
 	"github.com/iotexproject/iotex-core/blockchain/block"
-	"github.com/pkg/errors"
-	"github.com/shopspring/decimal"
-	"gorm.io/gorm"
+	slog "github.com/iotexproject/iotex-core/pkg/log"
 )
 
 const VERSION = "2.1.3"
@@ -23,6 +31,10 @@ type income struct {
 	outNumActions int
 }
 type accountIncomeV1Plugin struct {
+	batchSize             int
+	tipHeight             uint64
+	accountIncome         []*AccountIncomeV1
+	accountIncomeCountMap map[string]*AccountIncomeCountV1
 }
 
 func (b accountIncomeV1Plugin) Name() string {
@@ -33,7 +45,23 @@ func (b accountIncomeV1Plugin) Type() plugin.Type {
 	return plugin.TypeStandard
 }
 
-func (b accountIncomeV1Plugin) Start(ctx context.Context) error {
+func (b accountIncomeV1Plugin) BatchSize() uint64 {
+	return uint64(b.batchSize)
+}
+
+func (b *accountIncomeV1Plugin) Start(ctx context.Context) error {
+	b.accountIncomeCountMap = make(map[string]*AccountIncomeCountV1)
+
+	if cfgData, ok := kernel.GetPluginConfigCtx(ctx); ok {
+		cfg := &Config{}
+		if err := yaml.Unmarshal(cfgData, cfg); err != nil {
+			return errors.Wrapf(err, "failed to unmarshal plugin config: plugin %s, config %s", b.Name(), string(cfgData))
+		} else {
+			b.batchSize = cfg.BatchSize
+			slog.L().Info("read plugin config success", zap.String("plugin", b.Name()), zap.Any("config", cfg))
+		}
+	}
+
 	var ai *AccountIncomeV1
 	var aic *AccountIncomeCountV1
 	if err := db.AutoMigrate(b.Name(), &AccountIncomeV1{}, &AccountIncomeCountV1{}); err != nil {
@@ -128,57 +156,118 @@ func getIncomes(blk *block.Block) (map[string]income, error) {
 	}
 	return incomes, nil
 }
+
+func (b *accountIncomeV1Plugin) PutBlocks(ctx context.Context, blks []*block.Block) error {
+	slog.L().Info("PutBlocks ", zap.String("plugin", b.Name()), zap.Int("blocks", len(b.accountIncome)))
+	slog.L().Info("PutBlocks ", zap.String("plugin", b.Name()), zap.Int("actions", len(b.accountIncomeCountMap)))
+	startTime := float64(time.Now().UnixNano()) / 1e9
+	for _, blk := range blks {
+		if err := b.putBlock(ctx, blk); err != nil {
+			return err
+		}
+	}
+	slog.L().Info("PutBlocks end", zap.String("plugin", b.Name()), zap.Int("actions", len(b.accountIncomeCountMap)))
+
+	server.OpDurationMtc.WithLabelValues("account_income_v1", "putBlocks").Set(float64(time.Now().UnixNano())/1e9 - startTime)
+	b.tipHeight = blks[0].Height() + uint64(len(blks))
+	err := b.commit()
+	slog.L().Info("PutBlocks end commit", zap.String("plugin", b.Name()), zap.Int("actions", len(b.accountIncomeCountMap)))
+	return err
+}
+
 func (b accountIncomeV1Plugin) PutBlock(ctx context.Context, blk *block.Block) error {
+	if err := b.putBlock(ctx, blk); err != nil {
+		return err
+	}
+	b.tipHeight = blk.Height()
+	return b.commit()
+}
+
+func (b *accountIncomeV1Plugin) putBlock(ctx context.Context, blk *block.Block) error {
 	incomes, err := getIncomes(blk)
 	if err != nil {
 		return err
 	}
 	blkHeight := blk.Height()
-	err = db.DB().Transaction(func(tx *gorm.DB) error {
-		for accountAddress, accountIncome := range incomes {
-			inFlow := decimal.NewFromBigInt(accountIncome.inFlow, 0)
-			outFlow := decimal.NewFromBigInt(accountIncome.outFlow, 0)
-			aim := &AccountIncomeV1{
-				BlockHeight:   blkHeight,
-				Address:       accountAddress,
-				InFlow:        inFlow,
-				InNumActions:  accountIncome.inNumActions,
-				OutFlow:       outFlow,
-				OutNumActions: accountIncome.outNumActions,
-			}
-			if err := tx.Create(aim).Error; err != nil {
+	for accountAddress, accountIncome := range incomes {
+		inFlow := decimal.NewFromBigInt(accountIncome.inFlow, 0)
+		outFlow := decimal.NewFromBigInt(accountIncome.outFlow, 0)
+		b.accountIncome = append(b.accountIncome, &AccountIncomeV1{
+			BlockHeight:   blkHeight,
+			Address:       accountAddress,
+			InFlow:        inFlow,
+			InNumActions:  accountIncome.inNumActions,
+			OutFlow:       outFlow,
+			OutNumActions: accountIncome.outNumActions,
+		})
+		aicm := &AccountIncomeCountV1{
+			Address:       accountAddress,
+			InFlow:        inFlow,
+			InNumActions:  accountIncome.inNumActions,
+			OutFlow:       outFlow,
+			OutNumActions: accountIncome.outNumActions,
+		}
+		if err := b.appendToAccountIncomeCount(aicm); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (b *accountIncomeV1Plugin) commit() error {
+	startTime := float64(time.Now().UnixNano()) / 1e9
+	err := db.DB().Transaction(func(tx *gorm.DB) error {
+		if len(b.accountIncome) > 0 {
+			if err := db.DB().Model(&AccountIncomeV1{}).CreateInBatches(b.accountIncome, len(b.accountIncome)+1).Error; err != nil {
+				slog.L().Error("put accountIncome ", zap.String("plugin", b.Name()), zap.Int("size", len(b.accountIncome)))
+				b.accountIncome = b.accountIncome[:0]
 				return err
 			}
-			aicm := &AccountIncomeCountV1{
-				Address:       accountAddress,
-				InFlow:        inFlow,
-				InNumActions:  accountIncome.inNumActions,
-				OutFlow:       outFlow,
-				OutNumActions: accountIncome.outNumActions,
-			}
-			var aic *AccountIncomeCountV1
-			if err := tx.Where("address = ?", accountAddress).First(&aic).Error; err != nil {
-				if err != gorm.ErrRecordNotFound {
-					return err
-				}
-				if err := tx.Create(aicm).Error; err != nil {
-					return err
-				}
-			} else {
-				aic.InFlow = aic.InFlow.Add(inFlow)
-				aic.InNumActions += accountIncome.inNumActions
-				aic.OutFlow = aic.OutFlow.Add(outFlow)
-				aic.OutNumActions += accountIncome.outNumActions
-
-				if err := tx.Save(aic).Error; err != nil {
-					return err
-				}
-			}
+			b.accountIncome = b.accountIncome[:0]
 		}
-		return db.UpdateIndexHeightByTx(tx, b.Name(), blkHeight)
-	})
+		if len(b.accountIncomeCountMap) > 0 {
+			accountIncomeCounts := make([]*AccountIncomeCountV1, 0, len(b.accountIncomeCountMap))
+			for _, v := range b.accountIncomeCountMap {
+				accountIncomeCounts = append(accountIncomeCounts, v)
+			}
+			if err := db.DB().Model(&AccountIncomeCountV1{}).Clauses(clause.OnConflict{
+				Columns:   []clause.Column{{Name: "id"}},
+				DoUpdates: clause.AssignmentColumns([]string{"address", "in_flow", "in_num_actions", "out_flow", "out_num_actions"}),
+			}).CreateInBatches(accountIncomeCounts, len(accountIncomeCounts)+1).Error; err != nil {
+				slog.L().Error("put accountIncomeCounts ", zap.String("plugin", b.Name()), zap.Int("size", len(accountIncomeCounts)))
+				b.accountIncomeCountMap = make(map[string]*AccountIncomeCountV1)
+				return err
+			}
+			b.accountIncomeCountMap = make(map[string]*AccountIncomeCountV1)
+		}
 
+		return db.UpdateIndexHeightByTx(tx, b.Name(), b.tipHeight)
+	})
+	server.OpDurationMtc.WithLabelValues("account_income_v1", "commit").Set(float64(time.Now().UnixNano())/1e9 - startTime)
 	return err
+}
+
+func (b accountIncomeV1Plugin) appendToAccountIncomeCount(aicm *AccountIncomeCountV1) error {
+	var aic *AccountIncomeCountV1
+	accountIncomeCount, ok := b.accountIncomeCountMap[aicm.Address]
+	if !ok {
+		if err := db.DB().Where("address = ?", aicm.Address).First(&aic).Error; err != nil {
+			if err != gorm.ErrRecordNotFound {
+				return err
+			}
+			b.accountIncomeCountMap[aicm.Address] = aicm
+			return nil
+		}
+		b.accountIncomeCountMap[aicm.Address] = aic
+		return nil
+	}
+	accountIncomeCount.InFlow = accountIncomeCount.InFlow.Add(aicm.InFlow)
+	accountIncomeCount.InNumActions += aicm.InNumActions
+	accountIncomeCount.OutFlow = accountIncomeCount.OutFlow.Add(aicm.OutFlow)
+	accountIncomeCount.OutNumActions += aicm.OutNumActions
+
+	return nil
 }
 
 func (b accountIncomeV1Plugin) Stop(ctx context.Context) error {

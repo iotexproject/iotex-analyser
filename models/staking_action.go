466792a3c625181f390b3d8dd53e068e337d7ebd
envestcc <chen1233216@hotmail.com>
2024-12-09 19:04:15 +0800

    staking_actions plugin to ch

--- models/staking_action.go
+++ models/staking_action.go
@@ -1,21 +1,35 @@
 package models
 
-import (
-	"github.com/shopspring/decimal"
+var StakingActionsDDL = `CREATE TABLE IF NOT EXISTS staking_actions
+(
+	block_height UInt64 NOT NULL,
+	bucket_id String NOT NULL,
+	owner_address FixedString(41) NOT NULL,
+	candidate FixedString(41) NOT NULL,
+	amount String NOT NULL,
+	act_type String NOT NULL,
+	sender FixedString(41) NOT NULL,
+	act_hash FixedString(64) NOT NULL,
+	auto_stake Bool NOT NULL,
+	duration UInt32 NOT NULL,
+	log_index UInt32 NOT NULL
 )
+ENGINE = ReplacingMergeTree()
+PRIMARY KEY (bucket_id, block_height, log_index)
+ORDER BY (bucket_id, block_height, log_index)`
 
 type StakingActions struct {
-	ID           uint64          `gorm:"primary_key;" sql:"type:bigint"`
-	BlockHeight  uint64          `gorm:"unsigned;index" sql:"type:bigint"`
-	BucketID     uint64          `gorm:"unsigned;index"`
-	OwnerAddress string          `gorm:"size:42;not null;default:'';index:,length:9"`
-	Candidate    string          `gorm:"size:42;not null;default:'';index:,length:9"`
-	Amount       decimal.Decimal `gorm:"type:decimal(42,0);not null;default:0;"`
-	ActType      string          `gorm:"size:42;not null;default:'';index"`
-	Sender       string          `gorm:"size:42;not null;default:'';index:,length:9"`
-	ActHash      string
-	AutoStake    bool
-	Duration     uint32
+	BlockHeight  uint64 `ch:"block_height"`
+	BucketID     string `ch:"bucket_id"`
+	OwnerAddress string `ch:"owner_address"`
+	Candidate    string `ch:"candidate"`
+	Amount       string `ch:"amount"`
+	ActType      string `ch:"act_type"`
+	Sender       string `ch:"sender"`
+	ActHash      string `ch:"act_hash"`
+	AutoStake    bool   `ch:"auto_stake"`
+	Duration     uint32 `ch:"duration"`
+	LogIndex     uint32 `ch:"log_index"`
 }
 
 func (StakingActions) TableName() string {
--- plugins/staking_actions/util.go
+++ plugins/staking_actions/util.go
@@ -1,121 +1,74 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/hex"
+	"context"
+	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/iotexproject/go-pkgs/hash"
-	"github.com/iotexproject/iotex-address/address"
-	"github.com/iotexproject/iotex-analyser/db"
+	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
 	"github.com/iotexproject/iotex-analyser/models"
+	"github.com/pkg/errors"
 	"github.com/shopspring/decimal"
-	"gorm.io/gorm"
 )
 
 func getCandidateAddressByName(name string, height uint64) (string, error) {
 	candidate := &models.Candidate{}
 	if err := candidate.FetchByNameWithHeight(name, height); err != nil {
-		return "", err
+		return "", errors.Wrap(err, "failed to fetch candidate by name")
 	}
 	return candidate.CandidateID, nil
 }
 
-func getBucketSumAmountByBucketID(tx *gorm.DB, bucketID uint64) (decimal.Decimal, error) {
-	var amount sql.NullString
+func getBucketSumAmountByBucketID(ctx context.Context, conn driver.Conn, bucketID string) (decimal.Decimal, error) {
+	amount := big.NewInt(0)
 	zero := decimal.NewFromInt(0)
-	if err := tx.Model(&models.StakingActions{}).Select("sum(amount)").Where("bucket_id=?", bucketID).Scan(&amount).Error; err != nil {
-		return zero, err
+	if err := conn.QueryRow(ctx, "SELECT sum(toUInt256(amount)) FROM ? WHERE bucket_id=?", models.StakingActions{}.TableName(), bucketID).Scan(&amount); err != nil {
+		return zero, errors.Wrap(err, "failed to get sum amount by bucket id")
 	}
-	if amount.String == "" {
+	if amount.Sign() == 0 {
 		return zero, nil
 	}
-	decmailAmount, err := decimal.NewFromString(amount.String)
+	decmailAmount, err := decimal.NewFromString(amount.String())
 	if err != nil {
-		return zero, err
+		return zero, errors.Wrap(err, "failed to convert string to decimal")
 	}
 	return decmailAmount, nil
 }
 
-func getFixBucketSumAmountByBucketID(tx *gorm.DB, bucketID uint64) (decimal.Decimal, error) {
-	var amount sql.NullString
-	var count int64
+func getFixBucketSumAmountByBucketID(ctx context.Context, tx driver.Conn, bucketID string) (decimal.Decimal, error) {
+	amount := big.NewInt(0)
+	var count uint64
 	zero := decimal.NewFromInt(0)
-	err := tx.Model(&models.StakingActions{}).Where("bucket_id=? and act_type='Unstake'", bucketID).Count(&count).Error
+	err := tx.QueryRow(ctx, "SELECT count(*) FROM ? WHERE bucket_id=? and act_type='Unstake'", models.StakingActions{}.TableName(), bucketID).Scan(&count)
 	if err != nil {
-		return zero, err
+		return zero, errors.Wrap(err, "failed to get count by bucket id")
 	}
 	if count == 0 {
 		return zero, nil
 	}
-	if err := tx.Model(&models.StakingActions{}).Select("sum(amount)").Where("bucket_id=? and act_type<>'Unstake'", bucketID).Scan(&amount).Error; err != nil {
-		return zero, err
+	if err := tx.QueryRow(ctx, "SELECT sum(toUInt256(amount)) FROM ? WHERE bucket_id=? and act_type!='Unstake'", models.StakingActions{}.TableName(), bucketID).Scan(&amount); err != nil {
+		return zero, errors.Wrap(err, "failed to get sum amount by bucket id")
 	}
-	if amount.String == "" {
+	if amount.Sign() == 0 {
 		return zero, nil
 	}
-	decmailAmount, err := decimal.NewFromString(amount.String)
+	decmailAmount, err := decimal.NewFromString(amount.String())
 	if err != nil {
-		return zero, err
+		return zero, errors.Wrap(err, "failed to convert string to decimal")
 	}
 	return decmailAmount, nil
 }
 
 type BucketInfo struct {
-	OwnerAddress string
-	Candidate    string
-	AutoStake    bool
-	Duration     uint32
+	OwnerAddress string `ch:"owner_address"`
+	Candidate    string `ch:"candidate"`
+	AutoStake    bool   `ch:"auto_stake"`
+	Duration     uint32 `ch:"duration"`
 }
 
-func getBucketInfoAddressByBucketID(tx *gorm.DB, bucketID uint64) (*BucketInfo, error) {
+func getBucketInfoAddressByBucketID(ctx context.Context, tx driver.Conn, bucketID string) (*BucketInfo, error) {
 	var bi BucketInfo
-	if err := tx.Model(&models.StakingActions{}).Select("owner_address,candidate,auto_stake,duration").Where("bucket_id=?", bucketID).Order("id desc").Limit(1).Scan(&bi).Error; err != nil {
-		return nil, err
+	if err := tx.QueryRow(ctx, "SELECT owner_address,candidate,auto_stake,duration FROM ? WHERE bucket_id=? ORDER BY block_height DESC, log_index DESC LIMIT 1", models.StakingActions{}.TableName(), bucketID).ScanStruct(&bi); err != nil {
+		return nil, errors.Wrap(err, "failed to get bucket info by bucket id")
 	}
 	return &bi, nil
-}
-
-func getAddresFromHash256(h hash.Hash256) (address.Address, error) {
-	hexStr := hex.EncodeToString(h[:])
-	ethAddr := hexStr[24:]
-	ethAddress := common.HexToAddress(ethAddr)
-	return address.FromBytes(ethAddress.Bytes())
-}
-
-func getBucketIDsByAddressWithHeight(addr string, height uint64) ([]uint64, error) {
-	db := db.DB()
-	var ids []struct {
-		BucketID uint64
-	}
-	if err := db.Model(&models.StakingActions{}).Distinct("bucket_id").Where("block_height<=? and owner_address=?", height, addr).Find(&ids).Error; err != nil {
-		return nil, err
-	}
-	bucketID := []uint64{}
-	for _, id := range ids {
-		bucketOwner, _ := getBucketOwnerWithHeight(id.BucketID, height)
-		if addr != bucketOwner {
-			continue
-		}
-		bucketID = append(bucketID, id.BucketID)
-	}
-	return bucketID, nil
-}
-
-func getBucketOwnerWithHeight(bucketID, height uint64) (string, error) {
-	var addr sql.NullString
-	db := db.DB()
-	if err := db.Model(&models.StakingActions{}).Select("owner_address").Where("block_height<=? and bucket_id=?", height, bucketID).Order("id desc").Limit(1).Scan(&addr).Error; err != nil {
-		return "", err
-	}
-	return addr.String, nil
-}
-
-func getForwardToAddressByFrom(addr string) (string, error) {
-	var to string
-	db := db.DB()
-	if err := db.Model(&models.StakingActions{}).Select("forward_to").Where("owner_address=? and act_type='GovernaceForward' and forward_to!=''", addr).Order("id desc").Limit(1).Scan(&to).Error; err != nil {
-		return "", err
-	}
-	return to, nil
 }

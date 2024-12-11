74bf75ade6b03c6b5cf943dc12388967352ba2ed
envestcc <chen1233216@hotmail.com>
2024-12-11 21:21:42 +0800

    verify candidate

--- cmd/tools/verifymigration/migration.go
+++ cmd/tools/verifymigration/migration.go
@@ -1,7 +1,16 @@
 package verifymigration
 
 import (
+	"fmt"
+	"strconv"
+
+	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+	"github.com/iotexproject/iotex-analyser/config"
+	"github.com/iotexproject/iotex-analyser/db"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/pkg/errors"
 	"github.com/urfave/cli/v2"
+	"gorm.io/gorm"
 )
 
 var VerifyMigration = &cli.Command{
@@ -9,6 +18,7 @@ var VerifyMigration = &cli.Command{
 	Usage: "verify_migration <subcommand>",
 	Subcommands: []*cli.Command{
 		VerifyActionExecutionCmd,
+		VerifyCandidateCmd,
 	},
 	Flags: []cli.Flag{
 		&cli.Uint64Flag{
@@ -32,4 +42,31 @@ var VerifyMigration = &cli.Command{
 			Value: "postgres://postgres:mysecretpassword@127.0.0.1:5432/testnet?sslmode=disable",
 		},
 	},
+}
+
+func connectDatabase(c *cli.Context) (driver.Conn, *gorm.DB, error) {
+	chDSN := c.String("chdsn")
+	pgDSN := c.String("pgdsn")
+	// verify action_execution
+	chConn, err := db.ConnectClickhouse(chDSN)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to connect clickhouse")
+	}
+	fmt.Println("clickhouse connected successfully", chDSN)
+	pgCfg, err := pgconn.ParseConfig(pgDSN)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to parse postgres dsn")
+	}
+	config.Default.Database.Driver = "postgres"
+	config.Default.Database.User = pgCfg.User
+	config.Default.Database.Password = pgCfg.Password
+	config.Default.Database.Host = pgCfg.Host
+	config.Default.Database.Name = pgCfg.Database
+	config.Default.Database.Port = strconv.FormatInt(int64(pgCfg.Port), 10)
+	pg, err := db.Connect()
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to connect postgres")
+	}
+	fmt.Println("postgres connected successfully", config.Default.Database.Host)
+	return chConn, pg, nil
 }

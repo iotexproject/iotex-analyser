8581362a90f62177bb686ffa644371d136424e87
envestcc <chen1233216@hotmail.com>
2024-12-11 21:36:47 +0800

    fix sum

--- plugins/staking_actions/util.go
+++ plugins/staking_actions/util.go
@@ -30,7 +30,7 @@ func bucketSumAmount(ctx context.Context, conn driver.Conn, bucketID string, sta
 func getBucketSumAmountByBucketID(ctx context.Context, conn driver.Conn, bucketID string) (decimal.Decimal, error) {
 	amount := big.NewInt(0)
 	zero := decimal.NewFromInt(0)
-	if err := conn.QueryRow(ctx, "SELECT sum(toUInt256(amount)) FROM ? WHERE bucket_id=?", models.StakingActions{}.TableName(), bucketID).Scan(&amount); err != nil {
+	if err := conn.QueryRow(ctx, "SELECT sum(toInt256(amount)) FROM ? WHERE bucket_id=?", models.StakingActions{}.TableName(), bucketID).Scan(&amount); err != nil {
 		return zero, errors.Wrap(err, "failed to get sum amount by bucket id")
 	}
 	if amount.Sign() == 0 {
@@ -54,7 +54,7 @@ func getFixBucketSumAmountByBucketID(ctx context.Context, tx driver.Conn, bucket
 	if count == 0 {
 		return zero, nil
 	}
-	if err := tx.QueryRow(ctx, "SELECT sum(toUInt256(amount)) FROM ? WHERE bucket_id=? and act_type!='Unstake'", models.StakingActions{}.TableName(), bucketID).Scan(&amount); err != nil {
+	if err := tx.QueryRow(ctx, "SELECT sum(toInt256(amount)) FROM ? WHERE bucket_id=? and act_type!='Unstake'", models.StakingActions{}.TableName(), bucketID).Scan(&amount); err != nil {
 		return zero, errors.Wrap(err, "failed to get sum amount by bucket id")
 	}
 	if amount.Sign() == 0 {

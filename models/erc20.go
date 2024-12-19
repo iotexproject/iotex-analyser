924bf2dc643bf3a8e8680ff754267fa6511fa9a7
huangzhiran <zhiran@iotex.me>
2024-12-19 15:54:33 +0800

    change erc20 DECIMAL type to string

--- models/erc20.go
+++ models/erc20.go
@@ -2,19 +2,17 @@ package models
 
 import (
 	"time"
-
-	"github.com/shopspring/decimal"
 )
 
 type Erc20Transfer struct {
-	ID              uint64          `gorm:"primary_key;uniqueIndex:idx_ca_id,priority:4;uniqueIndex:idx_ca_s_id,priority:5;uniqueIndex:idx_ca_r_id,priority:5;" sql:"type:bigint"`
-	BlockHeight     uint64          `gorm:"unsigned;index" sql:"type:bigint"`
-	ActionHash      string          `gorm:"size:64;not null;index:,length:9"`
-	ContractAddress string          `gorm:"size:42;not null;default:'';index:,length:9;uniqueIndex:idx_ca_id,priority:3;uniqueIndex:idx_ca_s_id,priority:3;uniqueIndex:idx_ca_r_id,priority:3;"`
-	Amount          decimal.Decimal `gorm:"type:decimal(128,0);not null;default:0;"`
-	Sender          string          `gorm:"size:42;not null;default:'';index:,length:9;uniqueIndex:idx_ca_s_id,priority:4;"`
-	Recipient       string          `gorm:"size:42;not null;default:'';index:,length:9;uniqueIndex:idx_ca_r_id,priority:4;"`
-	Timestamp       time.Time       `gorm:"type:timestamp;"`
+	ID              uint64    `gorm:"primary_key;uniqueIndex:idx_ca_id,priority:4;uniqueIndex:idx_ca_s_id,priority:5;uniqueIndex:idx_ca_r_id,priority:5;" sql:"type:bigint"`
+	BlockHeight     uint64    `gorm:"unsigned;index" sql:"type:bigint"`
+	ActionHash      string    `gorm:"size:64;not null;index:,length:9"`
+	ContractAddress string    `gorm:"size:42;not null;default:'';index:,length:9;uniqueIndex:idx_ca_id,priority:3;uniqueIndex:idx_ca_s_id,priority:3;uniqueIndex:idx_ca_r_id,priority:3;"`
+	Amount          string    `gorm:"not null;default:'';"`
+	Sender          string    `gorm:"size:42;not null;default:'';index:,length:9;uniqueIndex:idx_ca_s_id,priority:4;"`
+	Recipient       string    `gorm:"size:42;not null;default:'';index:,length:9;uniqueIndex:idx_ca_r_id,priority:4;"`
+	Timestamp       time.Time `gorm:"type:timestamp;"`
 }
 
 func (Erc20Transfer) TableName() string {
@@ -22,14 +20,14 @@ func (Erc20Transfer) TableName() string {
 }
 
 type Erc20Approval struct {
-	ID              uint64          `gorm:"primary_key;" sql:"type:bigint"`
-	BlockHeight     uint64          `gorm:"unsigned;index" sql:"type:bigint"`
-	ActionHash      string          `gorm:"size:64;not null;index:,length:9"`
-	ContractAddress string          `gorm:"size:42;not null;default:'';index:,length:9"`
-	Amount          decimal.Decimal `gorm:"type:decimal(128,0);not null;default:0;"`
-	Owner           string          `gorm:"size:42;not null;default:'';index:,length:9"`
-	Spender         string          `gorm:"size:42;not null;default:'';index:,length:9"`
-	Timestamp       time.Time       `gorm:"type:timestamp;"`
+	ID              uint64    `gorm:"primary_key;" sql:"type:bigint"`
+	BlockHeight     uint64    `gorm:"unsigned;index" sql:"type:bigint"`
+	ActionHash      string    `gorm:"size:64;not null;index:,length:9"`
+	ContractAddress string    `gorm:"size:42;not null;default:'';index:,length:9"`
+	Amount          string    `gorm:"not null;default:'';"`
+	Owner           string    `gorm:"size:42;not null;default:'';index:,length:9"`
+	Spender         string    `gorm:"size:42;not null;default:'';index:,length:9"`
+	Timestamp       time.Time `gorm:"type:timestamp;"`
 }
 
 func (Erc20Approval) TableName() string {
